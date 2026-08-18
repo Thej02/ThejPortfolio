d77a3420d4bbe4ba47429f1148c3676f7298815c
Thejaswi Nayak <kthejaswinayak@gmail.com>
2026-08-19 00:15:50 +0530

    fixed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -180,8 +180,8 @@ const AboutPage = () => {
               ========================= */}
 
               <a
-                href="/Thejaswi_Nayak_CV.pdf"
-                download="Thejaswi_Nayak_CV.pdf"
+                href="public/ThejaswiNayak_Resume .pdf"
+                download="ThejaswiNayak_Resume .pdf"
                 className="w-full lg:w-auto"
               >
 
